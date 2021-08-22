43784efca4c443382cd9e27bee30daab20b2d284
gaurigupta31 <gaurigupta.315@gmail.com>
2021-08-22 14:48:21 +0530

    added create table functions

--- cockroach.py
+++ cockroach.py
@@ -12,23 +12,54 @@ import json
 with open("secret.json") as f:
     secret = json.load(f)
 
-#connection with database
-try:
-    conn = psycopg2.connect(user=secret['DB_USER'],
+def checkConnection():
+    #connection with database
+    try:
+        conn = psycopg2.connect(user=secret['DB_USER'],
                                   password=secret['DB_PASS'],
                                   host=secret['DB_HOST'],
                                   port=secret['DB_PORT'],
                                   database=secret['DB_NAME'])
-    cur = conn.cursor()
+        print("Connected to SMU DB")
+        return conn
 
-except (Exception, psycopg2.Error) as error:
-    print("Failed to complete request", error)
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to complete request", error)
+        return False
 
-#closing database connection.
-finally:
+def closeConnection(conn):
+    #closing database connection.
     if conn:
-        cur.close()
         conn.close()
         print("PostgreSQL connection is closed")
 
+         
+def createTableSubscribers(sid):
+    conn = checkConnection()
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE _''' + sid + '''_subscribers(
+        id BIGSERIAL NOT NULL PRIMARY KEY, 
+        email VARCHAR(150) NOT NULL);''')
+    conn.commit()
+    closeConnection(conn)
 
+def createTableTemplates(sid):
+    conn = checkConnection()
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE _''' + sid + '''_templates(
+        id BIGSERIAL NOT NULL PRIMARY KEY, 
+        name VARCHAR(50) NOT NULL, 
+        link VARCHAR(50) NOT NULL);''')
+    conn.commit()
+    closeConnection(conn)
+
+# sid = server id
+# createTableSubscribers() #creates a table for subscribers with name _sid_subscribers
+# createTableTemplates() #creates a table for templates with name _sid_templates
+# checkConnection() # returns cursor object if connected and False if connection fails
+# closeConnection() # when all operations over :D   
+
+#for testing
+if __name__ == '__main__':
+    createTableTemplates("53")
+    createTableSubscribers("53")
